46401f7570122cfe7eee594b74dfe7492d563e28
DemianWHT <demiankost2@gmail.com>
2024-02-16 00:57:59 +0100

    Moved migrations to tenant folder and added fields for companies migration

--- database/migrations/tenant/2024_02_15_233627_create_companies_table.php
+++ database/migrations/tenant/2024_02_15_233627_create_companies_table.php
@@ -1,25 +1,28 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
-        Schema::create('companies', function (Blueprint $table) {
+        Schema::create('companies', function (Blueprint $table):void {
             $table->id();
+            $table->uuid('uuid')->unique();
+
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('phone');
+            $table->string('website');
+            
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('companies');
