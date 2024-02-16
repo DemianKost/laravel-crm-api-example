1c6965851ef7bc834406edaa055341da691bb43a
DemianWHT <demiankost2@gmail.com>
2024-02-16 17:27:38 +0100

    Set all logic and relations for all models

--- app/Models/Department.php
+++ app/Models/Department.php
@@ -1,11 +1,36 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Department extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    /**
+     * @var string[]
+     */
+    protected $fillable = [
+        'uuid',
+        'name',
+        'company_id',
+    ];
+
+    /**
+     * @return BelongsTo
+     */
+    public function company(): BelongsTo
+    {
+        return $this->belongsTo(
+            related: Company::class,
+            foreignKey: 'company_id',
+        );
+    }
 }
--- app/Models/Team.php
+++ app/Models/Team.php
@@ -1,11 +1,35 @@
 <?php
 
+declare(strict_types=1);
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Team extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    /**
+     * @var string[]
+     */
+    protected $fillable = [
+        'uuid',
+        'name',
+        'department_id'
+    ];
+
+    /**
+     * @return BelongsTo
+     */
+    public function department(): BelongsTo
+    {
+        return $this->belongsTo(
+            related: Department::class,
+            foreignKey: 'department_id',
+        );
+    }
 }
