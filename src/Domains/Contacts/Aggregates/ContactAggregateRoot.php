1b27cc593b657fe276efcf8bc89c4b99d7a25375
DemianKost <demiankost2@gmail.com>
2024-04-14 20:04:01 +0200

    Added spatie migrations and config of event sourcing

---
+++ src/Domains/Contacts/Aggregates/ContactAggregateRoot.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Domains\Contacts\Aggregates;
+use Spatie\EventSourcing\AggregateRoots\AggregateRoot;
+
+class ContactAggregateRoot extends AggregateRoot
+{
+    
+}
