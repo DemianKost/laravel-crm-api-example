d08cc372caaa28adc91f4a3d5c7fb09122faf39b
DemianKost <demiankost2@gmail.com>
2024-04-14 02:03:13 +0200

    Create ActionsTest.php

---
+++ tests/Feature/API/Contacts/ActionsTest.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Actions\Contacts\CreateNewContact;
+use App\Enums\Pronouns;
+use App\Factories\ContactFactory;
+use Illuminate\Database\Eloquent\Model;
+
+it( 'can create a new contact', function(string $string) {
+    expect(
+        CreateNewContact::handle(
+            object: ContactFactory::make(
+                attributes: [
+                    'title' => $string,
+                    'name' => [
+                        'first' => $string,
+                        'middle' => $string,
+                        'last' => $string,
+                        'preferred' => $string,
+                        'full' => "$string $string $string"
+                    ],
+                    'phone' => $string,
+                    'email' => "{$string}@gmail.com",
+                    'pronouns' => Pronouns::random(),
+                ],
+            ),
+        )
+    )->toBeInstanceOf(Model::class)->phone->toEqual($string);
+})->with('strings');
