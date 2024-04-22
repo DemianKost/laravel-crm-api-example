8fbc13d5a78c1b77c4adadf073d84dc19d400665
DemianKost <demiankost2@gmail.com>
2024-04-23 00:01:50 +0200

    Finally added test for testing event sourcing for interaction store controller

--- tests/Feature/API/Interactions/HandlersTest.php
+++ tests/Feature/API/Interactions/HandlersTest.php
@@ -1,2 +1,40 @@
 <?php
 
+declare(strict_types=1);
+
+use App\Models\Contact;
+use App\Models\Interaction;
+use Domains\Interactions\Enums\InteractionType;
+use Domains\Interactions\Events\InteractionWasCreated;
+use Domains\Interactions\Factories\InteractionFactory;
+use Domains\Interactions\Handlers\InteractionHandler;
+
+it( 'can store a new interaction', function() {
+    $event = new InteractionWasCreated(
+        object: InteractionFactory::make(
+            attributes: [
+                'type' => InteractionType::EMAIL->value,
+                'contact' => Contact::factory()->create()->id,
+                'content' => 'Some content',
+                'user' => null,
+                'project' => null,
+            ],
+        ),
+    );
+
+    expect(
+        $event,
+    )->toBeInstanceOf( class: InteractionWasCreated::class );
+
+    expect(
+        Interaction::query()->count(),
+    )->toEqual(0);
+
+    (new InteractionHandler())->onInteractionWasCreated(
+        event: $event,
+    );
+
+    expect(
+        Interaction::query()->count(),
+    )->toEqual(1);
+} );
