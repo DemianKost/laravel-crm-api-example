58952ec685612fb4eeeb7e00dc7912ae7b584b21
DemianKost <demiankost2@gmail.com>
2024-04-28 17:30:46 +0200

    Update ValueObjectsTest.php

--- tests/Unit/Interactions/ValueObjectsTest.php
+++ tests/Unit/Interactions/ValueObjectsTest.php
@@ -3,15 +3,29 @@
 declare(strict_types=1);
 
 use Domains\Interactions\Enums\InteractionType;
+use Domains\Interactions\Factories\InteractionFactory;
 use Domains\Interactions\ValueObjects\InteractionValueObject;
 
-it( 'can create a new interaction value object', function(string $string) {
+it( 'can create a new interaction value object', function( string $string ) {
     expect(
         new InteractionValueObject(
             type: InteractionType::EMAIL->value,
             contact: 1,
             user: 1,
             content: $string,
+        ),
+    )->toBeInstanceOf(InteractionValueObject::class);
+})->with('strings');
+
+it( 'can create a new interaction value object via interaction factory', function( string $string ) {
+    expect(
+        value: InteractionFactory::make(
+            attributes: [
+                'type' => InteractionType::EMAIL->value,
+                'contact' => 1,
+                'user' => 1,
+                'content' => $string,
+            ]
         ),
     )->toBeInstanceOf(InteractionValueObject::class);
 })->with('strings');
